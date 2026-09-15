adb9c46558bff5fedc5654ae63fe6dfc4e8a0474
ExoticAndrew <andrewantunes61@gmail.com>
2026-09-14 21:41:22 -0300

    debug: log temporario

--- src/main/java/exolex/exotic/ExoticApplication.java
+++ src/main/java/exolex/exotic/ExoticApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExoticApplication {
 
 	public static void main(String[] args) {
+		System.out.println("### DEBUG SPRING_PROFILES_ACTIVE = " + System.getenv("SPRING_PROFILES_ACTIVE"));
 		SpringApplication.run(ExoticApplication.class, args);
 	}
 
