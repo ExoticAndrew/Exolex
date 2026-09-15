937cb5099240777984ffdea7b49904a7c5f78e10
ExoticAndrew <andrewantunes61@gmail.com>
2026-09-14 21:45:08 -0300

    debug: log variaveis db

--- src/main/java/exolex/exotic/ExoticApplication.java
+++ src/main/java/exolex/exotic/ExoticApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExoticApplication {
 
 	public static void main(String[] args) {
-		System.out.println("### DEBUG SPRING_PROFILES_ACTIVE = " + System.getenv("SPRING_PROFILES_ACTIVE"));
+		System.out.println("### DEBUG DB_HOST = " + System.getenv("DB_HOST"));
+		System.out.println("### DEBUG DB_NAME = " + System.getenv("DB_NAME"));
+		System.out.println("### DEBUG DB_USER = " + System.getenv("DB_USER"));
 		SpringApplication.run(ExoticApplication.class, args);
 	}
 
